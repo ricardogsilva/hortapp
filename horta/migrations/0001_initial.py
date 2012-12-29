c6083e04a9eda27d4c7940aa5ac20fd2350f0cee
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2012-12-29 15:17:17 +0000

    working on the models

--- horta/migrations/0001_initial.py
+++ horta/migrations/0001_initial.py
@@ -8,13 +8,74 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Item'
+        db.create_table('horta_item', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('horta', ['Item'])
+
+        # Adding model 'GeoRepresentation'
+        db.create_table('horta_georepresentation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['horta.Item'])),
+        ))
+        db.send_create_signal('horta', ['GeoRepresentation'])
+
+        # Adding model 'PolygonRepresentation'
+        db.create_table('horta_polygonrepresentation', (
+            ('geo_representation', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['horta.GeoRepresentation'], unique=True, primary_key=True)),
+            ('geom', self.gf('django.contrib.gis.db.models.fields.PolygonField')()),
+        ))
+        db.send_create_signal('horta', ['PolygonRepresentation'])
+
+        # Adding model 'Zone'
+        db.create_table('horta_zone', (
+            ('item', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['horta.Item'], unique=True, primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='Zone', max_length=100)),
+        ))
+        db.send_create_signal('horta', ['Zone'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Item'
+        db.delete_table('horta_item')
+
+        # Deleting model 'GeoRepresentation'
+        db.delete_table('horta_georepresentation')
+
+        # Deleting model 'PolygonRepresentation'
+        db.delete_table('horta_polygonrepresentation')
+
+        # Deleting model 'Zone'
+        db.delete_table('horta_zone')
+
 
     models = {
-        
+        'horta.georepresentation': {
+            'Meta': {'object_name': 'GeoRepresentation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['horta.Item']"})
+        },
+        'horta.item': {
+            'Meta': {'object_name': 'Item'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'horta.polygonrepresentation': {
+            'Meta': {'object_name': 'PolygonRepresentation', '_ormbases': ['horta.GeoRepresentation']},
+            'geo_representation': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['horta.GeoRepresentation']", 'unique': 'True', 'primary_key': 'True'}),
+            'geom': ('django.contrib.gis.db.models.fields.PolygonField', [], {})
+        },
+        'horta.zone': {
+            'Meta': {'object_name': 'Zone', '_ormbases': ['horta.Item']},
+            'item': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['horta.Item']", 'unique': 'True', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'Zone'", 'max_length': '100'})
+        }
     }
 
     complete_apps = ['horta']
--- horta/models.py
+++ horta/models.py
@@ -1,21 +1,33 @@
-from django.db import models
-from django.contrib.gis.db import models as geo_models
+from django.contrib.gis.db import models
 
 class Item(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
-class GeoRepresentation(geo_models.Model):
-    item = geo_models.ForeignKey(Item)
+class GeoRepresentation(models.Model):
+    item = models.ForeignKey(Item)
 
 class PolygonRepresentation(GeoRepresentation):
-    objects = geo_models.GeoManager()
-    geom = geo_models.PolygonField()
+    geo_representation = models.OneToOneField(GeoRepresentation,
+                                              primary_key=True,
+                                              parent_link=True)
+    objects = models.GeoManager()
+    geom = models.PolygonField()
 
 class Zone(Item):
+    item = models.OneToOneField(Item, primary_key=True, parent_link=True)
     name = models.CharField(max_length=100, default='Zone')
 
 class Canteiro(Zone):
-    pass
+    zone = models.OneToOneField(Zone, primary_key=True, parent_link=True)
 
+class Especie(Item):
+    item = models.OneToOneField(Item, primary_key=True, parent_link=True)
+    name = models.CharField(max_length=100, default='unspecified')
+
+class Plantacao(models.Model):
+    especie = models.ForeignKey(Especie)
+    zone = models.ForeignKey(Zone)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
